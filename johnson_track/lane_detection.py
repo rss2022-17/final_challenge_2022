d3e5608e61765731012a146e7f281ffdf70f9f21
pocato3rd <rcato3rd@mit.edu>
2022-05-02 09:37:06 -0400

    update threshold 0 handling

--- johnson_track/lane_detection.py
+++ johnson_track/lane_detection.py
@@ -237,6 +237,9 @@ def hough_section(image, top_bound, bottom_bound, pinch, base_threshold, midline
         print("pos")
         print(threshold)
         threshold = int(threshold/2.0)
+
+        if not threshold: return None, None
+
         lines_pos_slope = cv2.HoughLines(section,1, np.pi/180.0,threshold,srn=0,stn=0,min_theta=0,max_theta=np.pi*0.4)
     
     threshold = base_threshold
@@ -244,6 +247,9 @@ def hough_section(image, top_bound, bottom_bound, pinch, base_threshold, midline
         print("neg")
         print(threshold)
         threshold = int(threshold/2.0)
+
+        if not threshold: return None, None
+
         lines_neg_slope = cv2.HoughLines(section,1, np.pi/180.0,threshold,srn=0,stn=0,min_theta=np.pi*0.6,max_theta=np.pi)
 
 
@@ -281,6 +287,9 @@ def get_trajectory(image):
     all_right_points = []
     
     closest_line_left, closest_line_right = hough_section(canny_edge_image,0.5,1.0, 0.6, 150, image.shape[1] / 2.0, image.copy())
+
+    if closest_line_left is None or closest_line_right is None: return np.array([0, 0])
+
     steps = [.9,.8,.7, .65, .6, .55, .5]
     left_points,right_points = trajectory_rails(image, closest_line_left,closest_line_right,steps)
     all_left_points.extend(left_points)
@@ -289,6 +298,8 @@ def get_trajectory(image):
     new_midline = (all_left_points[-1][0] + all_right_points[-1][0])/2.0
 
     closest_line_left, closest_line_right = hough_section(canny_edge_image,0.45,0.5, 0.48, 150, new_midline, image.copy())
+    if closest_line_left is None or closest_line_right is None: return np.array([0, 0])
+
     steps = [.49,.48,.47,.46,.45]
     left_points,right_points = trajectory_rails(image, closest_line_left,closest_line_right,steps)
     if (np.abs(all_left_points[-1][0] - left_points[0][0]) < 50) and (np.abs(all_right_points[-1][0] - right_points[0][0]) < 50):
@@ -298,6 +309,8 @@ def get_trajectory(image):
     new_midline = (all_left_points[-1][0] + all_right_points[-1][0])/2.0
 
     closest_line_left, closest_line_right = hough_section(canny_edge_image,0.4,0.45, 0.43, 50, new_midline, image.copy())
+    if closest_line_left is None or closest_line_right is None: return np.array([0, 0])
+
     steps = [.44,.43,.42,.41]
     left_points,right_points = trajectory_rails(image, closest_line_left,closest_line_right,steps)
     if (np.abs(all_left_points[-1][0] - left_points[0][0]) < 50) and (np.abs(all_right_points[-1][0] - right_points[0][0]) < 50):
