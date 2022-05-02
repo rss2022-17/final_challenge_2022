79b4bdf36935b882590fc470c90c0f4861c13d88
Sebastian <sebastianag2002@gmail.com>
2022-05-02 20:12:29 +0000

    modified stop detector

--- city_driving/stop_detector/stop_sign_controls.py
+++ city_driving/stop_detector/stop_sign_controls.py
@@ -9,12 +9,12 @@ from std_msgs.msg import Bool
 class StopSignControl():
 
     def __init__(self):
-        self.subscriber = rospy.Subscriber("stop_sign_bbox", Float32MultiArray, self.bbox_callback)
+        self.bbox_sub = rospy.Subscriber("stop_sign_bbox", Float32MultiArray, self.bbox_callback)
         self.sign_in_range= rospy.Publisher("stop_detected", Bool, queue_size = 1)
-        self.sign_height = 
+        #self.image_sub = rospy.Subscriber("stop_sign_debug", Image, self.image_callback)
+        
+        #Currently just a guess for the transformation/distance for testing purposes
 
-    #TODO: Implemen logic so that it doesn't send a signal to state machine,
-    #after it already has done it once
 
     """
     Callback method for bbox's
@@ -29,8 +29,10 @@ class StopSignControl():
         else:
             dist_to_stop = self.get_distance(bbox)
             #If between .75meters and 1 meter away, then send flag to stop
-            if dist_to_stop > .75 and dist_to_stop < 1:
+            if dist_to_stop > .75 and dist_to_stop < 1 or True: #remove the OR TRUE statement once we worry about distace
                 self.sign_in_range.publish(True)
+                rospy.sleep(.1)
+                self.sign_in_range.publish(False)
                 #Sleeps for 5 seconds which should be enough time for the car to have driven
                 #past the stop sign or atleast out of range to not consider it again
                 rospy.sleep(5)
@@ -38,15 +40,19 @@ class StopSignControl():
             else:
                 self.sign_in_range.publish(False)
 
-    #impement method that detects the distance between stop sign using some math, and publisihes true
+    #implement method that detects the distance between stop sign using some math, and publisihes true
     #when its in a certain range
 
+    """
+    Gets distance of object to camera
+    """
     def get_distance(self, bbox):
         xmin = bbox[0]
         ymin = bbox[1]
         xmax = bbox[2]
         ymax = bbox[3]
-        pass
+        #temporary until I figure out a way to do this
+        return 0
 
     
 
